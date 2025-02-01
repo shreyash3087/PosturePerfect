9a8214cb1c18c5310a5549ea5e1348c9cbf15c31
Nikhil dangi <nikhildangi1205@gmail.com>
2025-02-01 20:39:05 +0530

    Adding new feature

--- Machine Learning/AITrainer.py
+++ Machine Learning/AITrainer.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import PoseModule as pm
 import requests
-import pygame  # Import pygame for sound handling
+import pygame  
 
 cap = cv2.VideoCapture(0)
 
@@ -12,7 +12,6 @@ count = 0
 dir = 0
 pTime = 0
 
-# Initialize pygame mixer for playing sound
 pygame.mixer.init()
 song = pygame.mixer.Sound("sound.mp3")  
 
@@ -25,7 +24,35 @@ while True:
     lmList = detector.findPosition(img, False)
 
     if len(lmList) != 0:
-        angle = detector.findAngle(img, 12, 14, 16)
+        # For each exercise, we track two-hand movement
+        if detector.current_exercise == pm.ExerciseType.BICEP_CURL:
+            angle_right = detector.findAngle(img, 12, 14, 16)  # Right arm
+            angle_left = detector.findAngle(img, 11, 13, 15)   # Left arm
+            angle = (angle_right + angle_left) / 2
+            per_right = np.interp(angle_right, (30, 160), (0, 100))  
+            per_left = np.interp(angle_left, (30, 160), (0, 100))    
+
+            per = min(per_right, per_left)  
+
+        elif detector.current_exercise == pm.ExerciseType.PUSHUP:
+            angle_right = detector.findAngle(img, 12, 14, 16)  
+            angle_left = detector.findAngle(img, 11, 13, 15) 
+            angle = (angle_right + angle_left) / 2
+            per_right = np.interp(angle_right, (30, 160), (0, 100))  
+            per_left = np.interp(angle_left, (30, 160), (0, 100))    
+
+            per = min(per_right, per_left)  
+
+        elif detector.current_exercise == pm.ExerciseType.SQUAT:
+            angle_right_leg = detector.findAngle(img, 24, 26, 28)  # Right leg
+            angle_left_leg = detector.findAngle(img, 23, 25, 27)   # Left leg
+            angle = (angle_right + angle_left) / 2
+            per_right_leg = np.interp(angle_right_leg, (170, 90), (0, 100)) 
+            per_left_leg = np.interp(angle_left_leg, (170, 90), (0, 100))    
+
+            per = min(per_right_leg, per_left_leg)  
+
+        
         per = np.interp(angle, (210, 310), (0, 100))
         bar = np.interp(angle, (220, 310), (650, 100))
 
@@ -39,11 +66,11 @@ while True:
                 response = requests.post(url, json=data)
                 print(response.status_code, response.text)
 
-                # Play the song when a push-up is completed
-                if  count % 1 == 0:  
-                    song.play() 
+                if  count % 1 == 0:   
+                    song.stop()  
+                    song.play()  
                     print("Push-up completed! Song is playing!")
-
+        print(count)
         if per == 0:
             color = (0, 255, 0)
             if dir == 1:
@@ -52,7 +79,7 @@ while True:
                 data = {"count": count}
                 response = requests.post(url, json=data)
                 print(response.status_code, response.text)
-                if  count % 1 == 0:  # Play once for every complete push-up
+                if  count % 1 == 0: 
                     song.play()  
                     print("Push-up completed! Song is playing!")
         print(count)
@@ -65,6 +92,7 @@ while True:
         cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
         cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
                     (255, 0, 0), 25)
+        
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -76,4 +104,3 @@ while True:
     key = cv2.waitKey(10) & 0xFF
     if key == ord('q'):  # Press 'q' to quit
         break
-
